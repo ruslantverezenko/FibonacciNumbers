4e65d8ead9ba10474114bac73efc343191a9a6e9
Ruslan Tverezenko <68191748+ruslantverezenko@users.noreply.github.com>
2023-01-19 20:56:24 +0300

    ConsoleAppFibonacciNumbers
    Task: to request from the user the number of Fibonacci numbers that he would like to generate (calculate), and, in fact, to generate (calculate). During the generation process, write numbers to an array. After generation, output the calculated numbers.

---
+++ ConsoleAppFibonacciNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            fibonacciNumbers();
+            Console.ReadLine();
+
+        }
+        static void fibonacciNumbers()
+        {
+            Console.Write("enter the number of fibonacci numbers: ");
+            int numberOfFibonacciNumbers = int.Parse(Console.ReadLine());
+            int[] numbersFibonacciMassive = new int[numberOfFibonacciNumbers + 2];
+
+            numbersFibonacciMassive[0] = 1;
+            numbersFibonacciMassive[1] = 1;
+
+            for (int i = 2; i < numberOfFibonacciNumbers + 2; i++)
+            {
+                numbersFibonacciMassive[i] = numbersFibonacciMassive[i - 1] + numbersFibonacciMassive[i - 2];
+                Console.Write($" {numbersFibonacciMassive[i]}");
+            }
+        }
+    }
+}
